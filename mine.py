2bf172288c48e822cea42a0c303811195de6ec2b
Rudy Gutierrez <93001008+rudy-gm@users.noreply.github.com>
2024-08-27 22:50:03 -0400

    Update mine.py
    added print for Hello World

--- mine.py
+++ mine.py
@@ -1,5 +1,6 @@
 # Write a line of code that prints "Hello world!".
 
+print("Hello world!")
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
@@ -7,3 +8,4 @@ def hello_world():
   variable_name = "Hello world!"
   
   return variable_name
+
